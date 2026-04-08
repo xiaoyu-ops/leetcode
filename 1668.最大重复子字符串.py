b1352201873665119b53a2f92a7cec67d2fc5127
Zhuoyang Wu <wuzhuoyang252@gmail.com>
2026-04-09 00:00:53 +0800

    补充 但是有部分是误触

---
+++ 1668.最大重复子字符串.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=1668 lang=python3
+#
+# [1668] 最大重复子字符串
+#
+
+# @lc code=start
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        
+# @lc code=end
+
